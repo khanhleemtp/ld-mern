822723352fa922fe09dfe45b8feb059055f9c200
LdKhanh <khanhleemtp>
2020-08-04 04:57:38 +0700

    done

--- server.js
+++ server.js
@@ -7,17 +7,21 @@ const config = require('config');
 const questionsRoute = require('./routes/api/questions');
 const usersRoute = require('./routes/api/users');
 const authRoute = require('./routes/api/auth');
+
 const app = express();
 
 // body parser Middleware
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-// app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // DB config
 const db = config.get('mongoURI');
 
+
 // connect db
+// step-2
 mongoose.connect(process.env.mongoURI || db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => console.log('Mongo db connected'))
     .catch(err => console.log(err));
@@ -28,6 +32,7 @@ app.use('/api/users', usersRoute);
 app.use('/api/auth', authRoute);
 
 // Serve static assets if in production
+// step-3
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
@@ -37,6 +42,7 @@ if(process.env.NODE_ENV === 'production') {
     })
 }
 
-const port = process.env.PORT || 5000;
+// Step-1-
+const port = process.env.PORT || 5000; //6656
 
 app.listen(port, ()=> console.log(`Server started on ${port}`));
